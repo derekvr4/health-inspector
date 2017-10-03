6a504fee1794496bb561a72b41a808caeaf94bee
derekvr4 <derekvr4@gmail.com>
2017-10-02 20:58:57 -0700

    hello world

--- src/index.js
+++ src/index.js
@@ -3,4 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+const App = () => {
+
+    return (
+        <div>
+        <h4>Hello World!</h4>
+        <p>I love saying hello world!</p>
+        </div>
+    )
+}
+ReactDOM.render(<App />,document.getElementById('root'));
 registerServiceWorker();
